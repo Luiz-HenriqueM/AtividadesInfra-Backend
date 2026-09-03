4d9424c16ea16c9024e293e699a3f1561f46d6bd
LuizHenriqueM <103379724+Luiz-HenriqueM@users.noreply.github.com>
2026-09-02 22:23:34 -0300

    criando banco

--- minha_primeira_API/index.js
+++ minha_primeira_API/index.js
@@ -2,7 +2,42 @@ import express from 'express';
 
 const app = express();  //Primeira instancia do express
 
-app.get("/", function(req, res) {
+
+
+
+
+
+
+let meuPrimeiroLivro = {
+    id: 1,
+    dsTitulo: "O Senhor dos Aneis",
+    dsAutor: "JRR Tolkien",
+    fgDisponivel: true,
+};
+
+ //idLivro
+ // dsTitulo, identificador
+ // dsAutor, string
+ // dsEditora, string
+ // fgDisponivel, boolean
+
+
+
+
+let livros = [meuPrimeiroLivro] //Banco de Dados
+
+
+
+
+
+
+
+
+
+
+
+//estado + caminho + funcao
+app.get("/", function(req, res) { //rota raiz
     res.send("Seja bem vindo a gestão de livros")
 
 });
@@ -17,3 +52,6 @@ app.get("/livros",  function(req, res) {
 });
 
 app.listen(3000); //Terceiro Pilar, porta a ser ouvida
+
+
+//CRUD(Post, Get, Put/Patch, Delete)
